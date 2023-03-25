99c3d8dfa054941f81d1590d78880fdc62e17f18
lx78WyY0J5 <9j2k37st@gmail.com>
2023-03-26 00:38:35 +0100

    Create ln-s.sh

---
+++ Serveurs/DataFolder/ln-s.sh
@@ -0,0 +1,50 @@
+ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/Creatif/plugins
+ln -s /Serveurs/DataFolder/plugins/Plugin-0.jar /Serveurs/Creatif/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/Creatif/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaVersion.jar /Serveurs/Creatif/plugins
+ln -s /Serveurs/DataFolder/plugins/worldedit.jar /Serveurs/Creatif/plugins
+ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Creatif
+
+ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/Hub/plugins
+ln -s /Serveurs/DataFolder/plugins/Plugin-0.jar /Serveurs/Hub/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/Hub/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaVersion.jar /Serveurs/Hub/plugins
+ln -s /Serveurs/DataFolder/plugins/worldedit.jar /Serveurs/Hub/plugins
+ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Hub
+
+ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/RPG/plugins
+ln -s /Serveurs/DataFolder/plugins/Plugin-0.jar /Serveurs/RPG/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/RPG/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaVersion.jar /Serveurs/RPG/plugins
+ln -s /Serveurs/DataFolder/plugins/worldedit.jar /Serveurs/RPG/plugins
+ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/RPG
+
+ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/SkyBlock/plugins
+ln -s /Serveurs/DataFolder/plugins/Plugin-0.jar /Serveurs/SkyBlock/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/SkyBlock/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaVersion.jar /Serveurs/SkyBlock/plugins
+ln -s /Serveurs/DataFolder/plugins/worldedit.jar /Serveurs/SkyBlock/plugins
+ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/SkyBlock
+
+ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/SUHC/plugins
+ln -s /Serveurs/DataFolder/plugins/Plugin-0.jar /Serveurs/SUHC/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/SUHC/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaVersion.jar /Serveurs/SUHC/plugins
+ln -s /Serveurs/DataFolder/plugins/worldedit.jar /Serveurs/SUHC/plugins
+ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/SUHC
+
+ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/Survie/plugins
+ln -s /Serveurs/DataFolder/plugins/Plugin-0.jar /Serveurs/Survie/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/Survie/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaVersion.jar /Serveurs/Survie/plugins
+ln -s /Serveurs/DataFolder/plugins/worldedit.jar /Serveurs/Survie/plugins
+ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Survie
+
+ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Waterfall
+
+ln -s /Serveurs/DataFolder/paper.jar /Serveurs/Creatif
+ln -s /Serveurs/DataFolder/paper.jar /Serveurs/Hub
+ln -s /Serveurs/DataFolder/paper.jar /Serveurs/RPG
+ln -s /Serveurs/DataFolder/paper.jar /Serveurs/SkyBlock
+ln -s /Serveurs/DataFolder/paper.jar /Serveurs/SUHC
+ln -s /Serveurs/DataFolder/paper.jar /Serveurs/Survie
