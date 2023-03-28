8de15a0bcf76b404047b935309db8fd5303bf99a
lx78WyY0J5 <9j2k37st@gmail.com>
2023-03-28 21:05:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 /Serveurs/Hub/start.sh
 /Serveurs/SkyBlock/start.sh
 /Serveurs/Survie/start.sh
+
+wget -O /DiscordBot/bot.jar 'https://github.com/GHub-fr/bot/releases/latest/download/bot-1.0-SNAPSHOT-fat.jar'
 /DiscordBot/start.sh
