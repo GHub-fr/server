c99c371005ca034c6f7c366392aef9ab2abe65c8
lx78WyY0J5 <9j2k37st@gmail.com>
2023-03-29 19:13:45 +0200

    Update start.sh for new bot link

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 /Serveurs/SkyBlock/start.sh
 /Serveurs/Survie/start.sh
 
-wget -O /DiscordBot/bot.jar 'https://github.com/GHub-fr/bot/releases/latest/download/bot-1.0-SNAPSHOT-fat.jar'
+wget -O /DiscordBot/bot.jar 'https://github.com/GHub-fr/bot/releases/latest/download/bot-1.0-fat.jar'
 /DiscordBot/start.sh
