11ac5a07ccb6c8be03b7bf20b4f084a60e3c338e
lx78WyY0J5 <9j2k37st@gmail.com>
2023-04-05 00:15:14 +0200

    Update setup.mysql.sh

--- setup.mysql.sh
+++ setup.mysql.sh
@@ -1,6 +1,7 @@
 # MySql
-PWD="PASSWORD"
+DB_PWD="PASSWORD"
+DB_NAME="site"
 #
-mysql -e "CREATE USER 'site'@'127.0.0.1' IDENTIFIED BY '${PWD}';"
-mysql -e "CREATE DATABASE dbsite;"
+mysql -e "CREATE USER 'site'@'127.0.0.1' IDENTIFIED BY '${DB_PWD}';"
+mysql -e "CREATE DATABASE ${DB_NAME};"
 mysql -e "GRANT ALL PRIVILEGES ON dbsite.* TO 'site'@'127.0.0.1' WITH GRANT OPTION;"
