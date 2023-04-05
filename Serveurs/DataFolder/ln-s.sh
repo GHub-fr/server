9a497e8a5b036cd28a1219c0897f19801ef82aae
lx78WyY0J5 <9j2k37st@gmail.com>
2023-04-05 13:29:53 +0200

    ln-s shell +mkdir

--- Serveurs/DataFolder/ln-s.sh
+++ Serveurs/DataFolder/ln-s.sh
@@ -1,3 +1,4 @@
+mkdir /Serveurs/Creatif/plugins
 ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/Creatif/plugins
 ln -s /Serveurs/DataFolder/plugins/plugin.jar /Serveurs/Creatif/plugins
 ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/Creatif/plugins
@@ -7,6 +8,7 @@ ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Creatif
 ln -s /Serveurs/DataFolder/eula.txt /Serveurs/Creatif
 ln -s /Serveurs/DataFolder/ops.json /Serveurs/Creatif
 
+mkdir /Serveurs/Hub/plugins
 ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/Hub/plugins
 ln -s /Serveurs/DataFolder/plugins/plugin.jar /Serveurs/Hub/plugins
 ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/Hub/plugins
@@ -16,6 +18,7 @@ ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Hub
 ln -s /Serveurs/DataFolder/eula.txt /Serveurs/Hub
 ln -s /Serveurs/DataFolder/ops.json /Serveurs/Hub
 
+mkdir /Serveurs/RPG/plugins
 ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/RPG/plugins
 ln -s /Serveurs/DataFolder/plugins/plugin.jar /Serveurs/RPG/plugins
 ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/RPG/plugins
@@ -25,6 +28,7 @@ ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/RPG
 ln -s /Serveurs/DataFolder/eula.txt /Serveurs/RPG
 ln -s /Serveurs/DataFolder/ops.json /Serveurs/RPG
 
+mkdir /Serveurs/SkyBlock/plugins
 ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/SkyBlock/plugins
 ln -s /Serveurs/DataFolder/plugins/plugin.jar /Serveurs/SkyBlock/plugins
 ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/SkyBlock/plugins
@@ -34,15 +38,17 @@ ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/SkyBlock
 ln -s /Serveurs/DataFolder/eula.txt /Serveurs/SkyBlock
 ln -s /Serveurs/DataFolder/ops.json /Serveurs/SkyBlock
 
-ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/SUHC/plugins
-ln -s /Serveurs/DataFolder/plugins/plugin.jar /Serveurs/SUHC/plugins
-ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/SUHC/plugins
-ln -s /Serveurs/DataFolder/plugins/ViaVersion.jar /Serveurs/SUHC/plugins
-ln -s /Serveurs/DataFolder/plugins/worldedit.jar /Serveurs/SUHC/plugins
-ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/SUHC
-ln -s /Serveurs/DataFolder/eula.txt /Serveurs/SUHC
-ln -s /Serveurs/DataFolder/ops.json /Serveurs/SUHC
+mkdir /Serveurs/Anarchie/plugins
+ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/Anarchie/plugins
+ln -s /Serveurs/DataFolder/plugins/plugin.jar /Serveurs/Anarchie/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/Anarchie/plugins
+ln -s /Serveurs/DataFolder/plugins/ViaVersion.jar /Serveurs/Anarchie/plugins
+ln -s /Serveurs/DataFolder/plugins/worldedit.jar /Serveurs/Anarchie/plugins
+ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Anarchie
+ln -s /Serveurs/DataFolder/eula.txt /Serveurs/Anarchie
+ln -s /Serveurs/DataFolder/ops.json /Serveurs/Anarchie
 
+mkdir /Serveurs/Survie/plugins
 ln -s /Serveurs/DataFolder/plugins/ImageOnMap.jar /Serveurs/Survie/plugins
 ln -s /Serveurs/DataFolder/plugins/plugin.jar /Serveurs/Survie/plugins
 ln -s /Serveurs/DataFolder/plugins/ViaBackwards.jar /Serveurs/Survie/plugins
@@ -52,6 +58,7 @@ ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Survie
 ln -s /Serveurs/DataFolder/eula.txt /Serveurs/Survie
 ln -s /Serveurs/DataFolder/ops.json /Serveurs/Survie
 
+mkdir /Serveurs/Waterfall
 ln -s /Serveurs/DataFolder/server-icon.png /Serveurs/Waterfall
 ln -s /Serveurs/DataFolder/eula.txt /Serveurs/Waterfall
 
@@ -59,5 +66,5 @@ ln -s /Serveurs/DataFolder/paper.jar /Serveurs/Creatif
 ln -s /Serveurs/DataFolder/paper.jar /Serveurs/Hub
 ln -s /Serveurs/DataFolder/paper.jar /Serveurs/RPG
 ln -s /Serveurs/DataFolder/paper.jar /Serveurs/SkyBlock
-ln -s /Serveurs/DataFolder/paper.jar /Serveurs/SUHC
+ln -s /Serveurs/DataFolder/paper.jar /Serveurs/Anarchie
 ln -s /Serveurs/DataFolder/paper.jar /Serveurs/Survie
