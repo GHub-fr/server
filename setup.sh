725e13487009672e99d805de250ba8300f6c2c00
lx78WyY0J5 <9j2k37st@gmail.com>
2023-04-08 23:43:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,23 @@ apt install -y wget
 
 
 wget -O /setup.mysql.sh 'https://raw.githubusercontent.com/GHub-fr/server/main/setup.mysql.sh'
+#
 wget -O /DiscordBot/data/tokens.yml 'https://raw.githubusercontent.com/GHub-fr/server/main/DiscordBot/data/tokens.yml'
 wget -O /Serveurs/DataFolder/tokens.yml 'https://raw.githubusercontent.com/GHub-fr/server/main/Serveurs/DataFolder/tokens.yml'
+#
+mkdir -p /Serveurs/Anarchie
+wget -O /Serveurs/Anarchie/server.properties 'https://raw.githubusercontent.com/GHub-fr/server/main/Serveurs/Anarchie/server.properties'
+mkdir -p /Serveurs/Creatif
+wget -O /Serveurs/Creatif/server.properties 'https://raw.githubusercontent.com/GHub-fr/server/main/Serveurs/Creatif/server.properties'
+mkdir -p /Serveurs/Hub
+wget -O /Serveurs/Hub/server.properties 'https://raw.githubusercontent.com/GHub-fr/server/main/Serveurs/Hub/server.properties'
+mkdir -p /Serveurs/RPG
+wget -O /Serveurs/RPG/server.properties 'https://raw.githubusercontent.com/GHub-fr/server/main/Serveurs/RPG/server.properties'
+mkdir -p /Serveurs/SkyBlock
+wget -O /Serveurs/SkyBlock/server.properties 'https://raw.githubusercontent.com/GHub-fr/server/main/Serveurs/SkyBlock/server.properties'
+mkdir -p /Serveurs/Survie
+wget -O /Serveurs/Survie/server.properties 'https://raw.githubusercontent.com/GHub-fr/server/main/Serveurs/Survie/server.properties'
+
 
 wget -O /install.sh 'https://raw.githubusercontent.com/GHub-fr/server/main/install.sh'
 chmod +x /install.sh
